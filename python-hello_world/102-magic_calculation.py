9aa9825f650398c576ef29a100f97b0aaeaa3237
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-06-24 06:36:06 -0700

    102-magic_calculation.py

--- python-hello_world/102-magic_calculation.py
+++ python-hello_world/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import dis
-def magic_calculation(a,b):
-    return a+b
 
-dis.dis(magic_calculation)
+
+def magic_calculation(a, b):
+    """Magic calculation."""
+    return 98 + a ** b
