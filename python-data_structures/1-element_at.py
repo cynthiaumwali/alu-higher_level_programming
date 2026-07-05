a30879e13fe4d06b4788c8ccc38c58c109f55564
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-05 08:40:13 -0700

    1-element_at.py

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for idx in range(len(my_list)):
+        if idx < 0:
+            print("None")
+        elif idx > 0: 
+            print("None")
+        else:
+            print("{}".format(my_list[idx]))
