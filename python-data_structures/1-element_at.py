3d0ff17f4fb9bbfe43c521d2256b41df8cf71d57
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-05 08:48:51 -0700

    update 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in range(len(my_list)):
-        if idx < 0:
-            print("None")
-        elif idx > len(my_list): 
-            print("None")
-        else:
-            print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
+    if idx < 0:
+        print("None")
+    elif idx > len(my_list): 
+        print("None")
+    else:
+        print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
