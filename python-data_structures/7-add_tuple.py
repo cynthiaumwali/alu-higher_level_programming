9ab669588e8aa1d21757e05bdd9c63c38f0bf4b0
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-06 01:27:35 -0700

    update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2 and len(tuple_b) < 2:
+    if not tuple_a and not tuple_b:
+        new_tuple = (0, 0)
+        return new_tuple
+    elif not tuple_a:
+        new_tuple = (tuple_b[0], tuple_b[1] if len(tuple_b) > 1 else 0)
+        return new_tuple
+    elif not tuple_b:
+        new_tuple = (tuple_a[0], tuple_a[1] if len(tuple_a) > 1 else 0)
+        return new_tuple
+    elif len(tuple_a) < 2 and len(tuple_b) < 2:
         new_tuple = ((tuple_a[0] + tuple_b[0]), 0)
         return new_tuple
     elif len(tuple_a) < 2:
@@ -9,13 +18,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) < 2:
         new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
         return new_tuple
-    elif not tuple_a and tuple_b:
-        new_tuple = (0, 0)
-        return new_tuple
-    elif not tuple_a:
-        return tuple_b
-    elif not tuple_b:
-        return tuple_a
     else:
         new_tuple = (tuple_a[0] + tuple_b[0], tuple_b[1] + tuple_a[1])
         return new_tuple
