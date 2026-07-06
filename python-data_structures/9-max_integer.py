c4b92d320791b2a237779fd46139ff1e1d23ab5e
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-06 02:51:05 -0700

    9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_number = 0
+    if not my_list:
+        return None
+    else:
+        for i in my_list:
+            if i > max_number:
+                max_number = i
+    return max_number
