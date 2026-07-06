0f22e38a211f1342d2c998608ec74314dc541f58
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-06 02:32:48 -0700

    update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     if sentence == "":
         print("Length: {:d} - First character: {}".format(0, None))
     else:
-        first_char = sentence[0]
         length = len(sentence)
+        first_char = sentence[0]
         print("Length: {:d} - First character: {}".format(length, first_char))
