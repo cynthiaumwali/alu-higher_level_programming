ca8466640c4c3a5654d58ee29366a8ac2d20699d
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-10 15:09:29 -0700

    update 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_value = list(a_dictionary.values())[0]
-    max_key = list(a_dictionary)[0]
     if not a_dictionary:
         return a_dictionary
+    max_value = list(a_dictionary.values())[0]
+    max_key = list(a_dictionary)[0]
     for key, value in a_dictionary.items():
         if a_dictionary[key] > max_value:
             max_value = a_dictionary[key]
