e5cbf4b84614cb1a4b1e420be5fb0c8843a967bd
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-10 08:04:21 -0700

    2-uniq_add.py

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    total_sum = 0
+    for idx in range(len(my_list)):
+        temp = my_list[idx]
+        occ = 0
+        for idxx in range(len(my_list)):
+            if idx == idxx: 
+                continue
+            else:
+                if my_list[idxx] == temp:
+                    occ += 1
+        if occ == 0:
+            total_sum += my_list[idx]
+    return total_sum
