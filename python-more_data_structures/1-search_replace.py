8eab0bb620f2f11fcb7346c2177407881ebbb101
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-10 06:47:40 -0700

    update 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,8 @@
 def search_replace(my_list, search, replace):
     if len(my_list) < 0:
         return my_list
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+    new_list = my_list
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
