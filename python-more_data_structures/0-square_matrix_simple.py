29faf6c44d0a33f3d3f14a7342de46be5e7e46e4
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-10 06:02:09 -0700

    update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     print("[", end="")
     for row in matrix:
         print("[", end="")
-        print(", ".join("{:d}".format(i) for i in row))
+        print(", ".join("{:d}".format(i) for i in row, end=""))
         print("]", end="")
     print("]", end="")
