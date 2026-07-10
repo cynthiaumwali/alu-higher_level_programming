cc47ac5bc56cc3974278d42ebc996899c4039977
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-10 14:22:48 -0700

    dictionary operations

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_value = list(a_dictionary.values())[0]
+    if not a_dictionary:
+        return a_dictionary
+    for key, value in a_dictionary.items():
+        if a_dictionary[key] > max_value:
+            max_value = a_dictionary[key]
+    return max_value
+
