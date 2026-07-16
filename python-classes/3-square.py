b72bc9248cf1a282de99c0ba77a4187853f97a23
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-16 19:30:38 +0000

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a Square"""
+
+
+    def __init__(self, size=0):
+        self.__size = size
+        """Initialize a Square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+    """Calculate Area"""
+        return size * size
