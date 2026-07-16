f7bc187ab5b168280582b3537a663aa8eaa55a77
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-16 20:14:31 +0000

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,6 +5,17 @@
 class Square:
     """Represents a Square"""
 
+    def size(self):
+        """Getter: for retrieving size"""
+        return self.__size
+
+    def size(self, value):
+        """Setter: for setting size value"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def __init__(self, size=0):
         """Initialize a Square"""
