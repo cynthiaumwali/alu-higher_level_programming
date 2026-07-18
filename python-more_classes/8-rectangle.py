582ba46b1aa39d3b74ffedc0956494aacd3a2cc1
cynthiaumwali <cynthiaumwali3@gmail.com>
2026-07-18 16:17:25 +0000

    8-rectangle.py

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -78,3 +78,17 @@ class Rectangle:
         """Print message when obj is deleted"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Rectangles comparison"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area > rect_2.area:
+            return rect_1
+        elif rect_1.area < rect_2.area:
+            return rect_2
+        else:
+            return rect_1
