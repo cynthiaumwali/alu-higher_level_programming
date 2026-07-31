7057a5c0c628a3061ecb920adb2abda0368cd127
ucynthia13 <cynthiaumwali3@gmail.com>
2026-07-31 21:42:57 +0200

    fix PEP8 Validation Errors

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -2,6 +2,8 @@
 """Square class that inherits from Rectangle."""
 
 Rectangle = __import__('9-rectangle').Rectangle
+
+
 class Square(Rectangle):
     """Represent a square."""
 
@@ -10,9 +12,11 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
     def area(self):
         """Return the area of the square."""
         return self.__size ** 2
+
     def __str__(self):
         """Return the string representation of the square."""
         return "[Square] {}/{}".format(self.__size, self.__size)
